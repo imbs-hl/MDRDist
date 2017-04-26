46afb797ad84aa3404c684e9e9bb0ff839ab438e
Johann Mattutat <johann.mattutat@student.uni-luebeck.de>
2017-04-26 15:18:47 +0200

    implementing cluster distance ratio in package

--- R/supporting_functions.R
+++ R/supporting_functions.R
@@ -45,3 +45,49 @@ wait_for_dir <- function(Dir, max_wait = 30, timeout = 1){
   # finally: if File has appeared, then continue. Abort otherwise.
   checkmate::assert_directory_exists(x = Dir)
 }
+
+
+#' Function to calculate ratio between cluster-intern and general distances
+#'
+#'
+#' @param real_classes the classes in which the observations are truely.
+#'    This value could either be a vector with integer coded classes or a
+#'    list of vectors containing multiple classes, if necessary
+#' @param distance_matrix an arbitrary distance matrix, that matches in size
+#'    and order of the observations with real_classes
+#'
+#' @return a numeric value that describes the ratio between cluster-intern and
+#'    general distances in the given distance matrix. Values approximately 0 imply
+#'    a good separation, values near 1 could be a sign of lacking informativity
+#' @export
+#'
+
+cluster_distance_ratio <- function(real_classes, distance_matrix){
+  existing_classes <- unique(unlist(real_classes))
+  elements_by_classes <- lapply(X = existing_classes,
+                                FUN = function(c, real_classes)
+                                {
+                                  res <- lapply(X = real_classes,
+                                                FUN = function(x, c){c %in% x},
+                                                c = c)
+                                  return(unlist(res))
+                                },
+                                real_classes = real_classes)
+
+  print(elements_by_classes)
+
+  class_intern_distances <- lapply(X = elements_by_classes,
+                                   FUN = function(x, distance_matrix)
+                                   {return(distance_matrix[x, x])},
+                                   distance_matrix = distance_matrix)
+
+  print(class_intern_distances)
+  cluster_intern_distance <- mean(unlist(class_intern_distances))
+  average_distance <- mean(distance_matrix)
+
+  return(cluster_intern_distance / average_distance)
+}
+
+
+
+
