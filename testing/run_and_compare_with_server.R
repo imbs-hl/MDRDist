2aa6838aeeb6291b76971b34169230abcd28a909
Johann Mattutat <johann.mattutat@student.uni-luebeck.de>
2017-07-05 12:51:59 +0200

    includes some legacy from testing to clean up

--- testing/run_and_compare_with_server.R
+++ testing/run_and_compare_with_server.R
@@ -1,13 +1,13 @@
-set.seed(1235)
+set.seed(1357)
 
 library(MDRDist)
 
-workdir   <- path.expand("~/Dokumente/MBMDR-Methodenvergleich/SANDBOX/HELLOWORLD/")
-scriptdir <- path.expand("~/Dokumente/MBMDR-Methodenvergleich/")
+workdir   <- path.expand("~/Dokumente/MBMDR-Methodenvergleich/SANDBOX/HELLOWORLD2/")
+scriptdir <- path.expand("~/git/MDRDist/testing/")
 
 source(file.path(scriptdir, "generate_population_functions.R"))
 source(file.path(scriptdir, "simulate_populations.R"))
 
 
-foo <- MDRDist::mdr_dist(data = Xnew, working_dir = workdir, n_rounds = 20)
+foo <- MDRDist::mdr_dist(data = Xnew, working_dir = workdir, n_rounds = 100)
 resultat <- cluster_distance_ratio(real_classes = Ynew, distance_matrix = foo$dist)
